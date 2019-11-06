5da051ddb8bf16492be16b126ce664cf69a95585
sssgordon <gso7@uwo.ca>
2019-11-06 15:40:00 +0100

    Added route to read a specific team by id

--- team/router.js
+++ team/router.js
@@ -1,14 +1,22 @@
 const { Router } = require("express");
 const Team = require("./model");
 const router = new Router();
+
 router.get("/team", (req, res, next) => {
   Team.findAll()
     .then(teamList => res.json(teamList)) // no need .send because the arrow function automatically sends by returning
     .catch(next);
 });
+
 router.post("/team", (req, res, next) => {
   Team.create(req.body)
     .then(newTeam => res.json(newTeam))
+    .catch(next);
+});
+
+router.get("/team/:id", (req, res, next) => {
+  Team.findByPk(req.params.id)
+    .then(team => res.json(team))
     .catch(next);
 });
 
